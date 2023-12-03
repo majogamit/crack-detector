ade92f4ced000ee6ef58e3e262a447ab496d9ce7
Mary Josephine Gamit <josephinegamit13@gmail.com>
2023-12-03 19:00:31 +0800

    Restructure base code and handle no crack error

--- app.py
+++ app.py
@@ -74,12 +74,10 @@ with gr.Blocks(theme=theme, css=css) as demo:
 
                 image_results = gr.Textbox(label="Result")
                 
-                # Display section for results
-                # md_result = gr.Markdown("**Results**", visible=False)
-                # csv_image = gr.File(label='CSV File', interactive=False, visible=False)
-                # df_image = gr.DataFrame(visible=False)
-        image_reference = gr.Image(sources=['upload', 'webcam'],
-                                       label='Reference Image',)
+        image_reference = gr.File(
+                    file_count="multiple",
+                    file_types=["image"],
+                    label="Reference Image",)
     # Video tab
     with gr.Tab("Video"):
         with gr.Row():
@@ -180,62 +178,6 @@ with gr.Blocks(theme=theme, css=css) as demo:
         return np.array(input_image)
     
     
-    
-    # def count_instance(result, filenames, uuid, width_list, orientation_list, image_path):
-    #     """
-    #     Counts the instances in the result and generates a CSV with the counts.
-
-    #     Parameters:
-    #         result (list): List containing results for each instance.
-    #         filenames (list): Corresponding filenames for each result.
-    #         uuid (str): Unique ID for the output folder name.
-    #         width_list (list): List containing width values for each instance.
-    #         orientation_list (list): List containing orientation values for each instance.
-
-    #     Returns:
-    #         tuple: Path to the generated CSV and dataframe with counts.
-    #     """
-    #     # Initializing the dataframe
-    #     data = {
-    #         'Index': [],
-    #         'FileName': [],
-    #         'Orientation': [],
-    #         'Width': [],
-    #         'Instance': []
-    #     }
-    #     df = pd.DataFrame(data)
-
-    #     # Populate the dataframe with counts, width, and orientation
-    #     for i, res in enumerate(result):
-    #         instance_count = len(res)
-    #         df.loc[i] = [i, os.path.basename(filenames[i]), orientation_list[i], width_list[i], instance_count]
-
-    #     # Save dataframe to a CSV file
-    #     path = os.path.join('output', uuid)
-    #     os.makedirs(path, exist_ok=True)
-    #     csv_filename = os.path.join(path, '_results.csv')
-
-    #     # Reorder columns
-    #     df = df[['Index', 'FileName', 'Orientation', 'Width', 'Instance']]
-
-    #     # Create a new dataframe (df2) with all columns from df
-    #     df2 = df.copy()
-
-    #     # Add another column for the image (modify as per your requirement)
-    #     image_column = [image_path[i].format(i) for i in range(len(df))]
-    #     df2['Image'] = image_column
-
-    #     # Save the modified dataframe to a CSV file
-    #     # df2.to_csv(csv_filename, index=False)
-    #     html_table = HTML(df.to_html(escape=False))
-    #     display(html_table)
-    #     return csv_filename, df2
-
-# Example usage:
-# count_instance(result, filenames, uuid, width_list, orientation_list)
-
-
-
     def predict_segmentation_im(image, conf, reference):
         """
         Perform segmentation prediction on a list of images.
@@ -247,6 +189,7 @@ with gr.Blocks(theme=theme, css=css) as demo:
         Returns:
             tuple: Paths of the processed images, CSV file, DataFrame, and Markdown.
         """
+        print("THE REFERENCE IN APPPY", reference)
         uuid = generate_uuid()
         image_list = [preprocess_image(Image.open(file.name)) for file in image]
         filenames = [file.name for file in image]
@@ -262,65 +205,67 @@ with gr.Blocks(theme=theme, css=css) as demo:
         for i, r in enumerate(results):
             result_list.append(r)
             instance_count = len(r)
-            # for m in r:
-            masks = r.masks.data
-            boxes = r.boxes.data
-            clss = boxes[:, 5]
-            people_indices = torch.where(clss == 0)
-            people_masks = masks[people_indices]
-            people_mask = torch.any(people_masks, dim=0).int() * 255
-            processed_image_path = str(model.predictor.save_dir / f'binarize{i}.jpg')
-            cv2.imwrite(processed_image_path, people_mask.cpu().numpy())
-            processed_image_paths.append(processed_image_path)
-            
-            crack_image_path = processed_image_path
-            principal_orientation, orientation_category = detect_pattern(crack_image_path)
-            
-            # Print the results if needed
-            print(f"Crack Detection Results for {crack_image_path}:")
-            print("Principal Component Analysis Orientation:", principal_orientation)
-            print("Orientation Category:", orientation_category)
+            if r.masks is not None and r.masks.data.numel() > 0:
+                masks = r.masks.data
+                boxes = r.boxes.data
+                clss = boxes[:, 5]
+                people_indices = torch.where(clss == 0)
+                people_masks = masks[people_indices]
+                people_mask = torch.any(people_masks, dim=0).int() * 255
+                processed_image_path = str(model.predictor.save_dir / f'binarize{i}.jpg')
+                cv2.imwrite(processed_image_path, people_mask.cpu().numpy())
+                processed_image_paths.append(processed_image_path)
+                
+                crack_image_path = processed_image_path
+                principal_orientation, orientation_category = detect_pattern(crack_image_path)
+                
+                # Print the results if needed
+                print(f"Crack Detection Results for {crack_image_path}:")
+                print("Principal Component Analysis Orientation:", principal_orientation)
+                print("Orientation Category:", orientation_category)
 
-            # Load the original image in color
-            original_img = cv2.imread(f'output/{uuid}/image{i}.jpg')
-            orig_image_path = str(model.predictor.save_dir / f'image{i}.jpg')
-            processed_image_paths.append(orig_image_path)
-            # Load and resize the binary image to match the dimensions of the original image
-            binary_image = cv2.imread(f'output/{uuid}/binarize{i}.jpg', cv2.IMREAD_GRAYSCALE)
-            binary_image = cv2.resize(binary_image, (original_img.shape[1], original_img.shape[0]))
+                # Load the original image in color
+                original_img = cv2.imread(f'output/{uuid}/image{i}.jpg')
+                orig_image_path = str(model.predictor.save_dir / f'image{i}.jpg')
+                processed_image_paths.append(orig_image_path)
+                # Load and resize the binary image to match the dimensions of the original image
+                binary_image = cv2.imread(f'output/{uuid}/binarize{i}.jpg', cv2.IMREAD_GRAYSCALE)
+                binary_image = cv2.resize(binary_image, (original_img.shape[1], original_img.shape[0]))
 
-            contour_analyzer = ContourAnalyzer()
-            max_width, thickest_section, thickest_points, distance_transforms = contour_analyzer.find_contours(binary_image)
+                contour_analyzer = ContourAnalyzer()
+                max_width, thickest_section, thickest_points, distance_transforms = contour_analyzer.find_contours(binary_image)
 
-            visualized_image = original_img.copy()
-            cv2.drawContours(visualized_image, [thickest_section], 0, (0, 255, 0), 1)
+                visualized_image = original_img.copy()
+                cv2.drawContours(visualized_image, [thickest_section], 0, (0, 255, 0), 1)
 
-            contour_analyzer.draw_circle_on_image(visualized_image, (int(thickest_points[0]), int(thickest_points[1])), 5, (57, 255, 20), -1)
-            print("Max Width in pixels: ", max_width)
+                contour_analyzer.draw_circle_on_image(visualized_image, (int(thickest_points[0]), int(thickest_points[1])), 5, (57, 255, 20), -1)
+                print("Max Width in pixels: ", max_width)
 
-            width = contour_analyzer.calculate_width(y=10, x=5, pixel_width=max_width, calibration_factor=0.001, distance=150)
-            print("Max Width, converted: ", width)
-            
-            visualized_image_path = f'output/{uuid}/visualized_image{i}.jpg'
-            output_image_paths.append(visualized_image_path)
-            cv2.imwrite(visualized_image_path, visualized_image)
+                width = contour_analyzer.calculate_width(y=10, x=5, pixel_width=max_width, calibration_factor=0.001, distance=150)
+                print("Max Width, converted: ", width)
+                
+                visualized_image_path = f'output/{uuid}/visualized_image{i}.jpg'
+                output_image_paths.append(visualized_image_path)
+                cv2.imwrite(visualized_image_path, visualized_image)
 
-            width_list.append(round(width, 2))
-            orientation_list.append(orientation_category)
+                width_list.append(round(width, 2))
+                orientation_list.append(orientation_category)
+            else:
+                original_img = cv2.imread(f'output/{uuid}/image{i}.jpg')
+                visualized_image_path = f'output/{uuid}/visualized_image{i}.jpg'
+                output_image_paths.append(visualized_image_path)
+                cv2.imwrite(visualized_image_path, original_img)
+                width_list.append('None')
+                orientation_list.append('None')
 
-            # Delete binarized and initial segmented images after processing
-            for path in processed_image_paths:
-                if os.path.exists(path):
-                    os.remove(path)
-            
-            res = f"Pattern: {orientation_category}\nWidth: {width}\nLength:\nCrack Instance: {instance_count}\nSafety Recommendation:"
+        # Delete binarized and initial segmented images after processing
+        for path in processed_image_paths:
+            if os.path.exists(path):
+                os.remove(path)
+                
         # results = gr.Textbox(res, visible=True)
-        csv, df = pt.count_instance(result_list, filenames, uuid, width_list, orientation_list, output_image_paths)
-        print(output_image_paths)
-        print(len(result_list))
-        print(filenames)
-        print("DF")
-        print(df)
+        csv, df = pt.count_instance(result_list, filenames, uuid, width_list, orientation_list, output_image_paths, reference)
+
         csv = gr.File(value=csv, visible=True)
         df = gr.DataFrame(value=df, visible=True)
         md = gr.Markdown(visible=True)
